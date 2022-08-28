bd2dbf40d82069758aa08f70c7087742bce83170
Prince Roy <princeku.ray143@gmail.com>
2022-08-29 01:03:18 +0530

    ajax call to geo IP

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,33 @@
 if (module.hot) {
     module.hot.accept();
 }
+
+const getLatLng = async() => {
+    const res = await fetch(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_k705Ppzs1oviTSMrmFKH6DI1OA2kP&ipAddress=192.212.174.101`
+    );
+
+    if (!res.ok) return;
+
+    const data = await res.json();
+    console.log(data);
+    return data.location;
+};
+const displayMap = async() => {
+    const { lat, lng } = await getLatLng();
+    const coords = [lat, lng];
+    console.log(coords);
+
+    const map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+};
+displayMap();
 console.log("hey");
