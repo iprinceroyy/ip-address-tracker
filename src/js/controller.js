6ea6ef957f28d085200c5bf8c21a7939becb79cb
Prince Roy <princeku.ray143@gmail.com>
2022-08-30 12:32:29 +0530

    update map marker

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,13 @@
+import markerIcon from '../images/icon-location.svg';
 if (module.hot) {
     module.hot.accept();
 }
 
-const getLatLng = async() => {
+const form = document.querySelector('.tracker__form');
+
+const getLatLng = async ip => {
     const res = await fetch(
-        `https://geo.ipify.org/api/v2/country,city?apiKey=at_k705Ppzs1oviTSMrmFKH6DI1OA2kP&ipAddress=192.212.174.101`
+        `https://geo.ipify.org/api/v2/country,city?apiKey=at_k705Ppzs1oviTSMrmFKH6DI1OA2kP&ipAddress=${ip}`
     );
 
     if (!res.ok) return;
@@ -13,21 +16,32 @@ const getLatLng = async() => {
     console.log(data);
     return data.location;
 };
-const displayMap = async() => {
-    const { lat, lng } = await getLatLng();
+const displayMap = async ip => {
+    const { lat, lng } = await getLatLng(ip);
     const coords = [lat, lng];
     console.log(coords);
 
-    const map = L.map("map").setView(coords, 13);
+    const map = L.map('map').setView(coords, 13);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-        .openPopup();
+    const myIcon = L.icon({
+        iconUrl: markerIcon,
+        iconSize: [50, 60],
+        iconAnchor: [30, 60],
+        popupAnchor: [-3, -76],
+    });
+
+    L.marker(coords, { icon: myIcon }).addTo(map);
+
+    L.marker.on('click', function(e) {
+        map.setView(e.latlng, 13);
+    });
 };
-displayMap();
-console.log("hey");
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const ip = e.target.firstElementChild.value;
+    displayMap('192.212.174.101');
+});
