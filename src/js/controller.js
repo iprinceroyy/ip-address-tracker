f8454c824bd1c0cb04c7f2d450d6ed6b8cc04ea6
Prince Roy <princeku.ray143@gmail.com>
2022-09-02 01:22:25 +0530

    refactor code into mvc architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,8 @@
-import markerIcon from '../images/icon-location.svg';
-import { getJSON } from './helpers';
 import * as model from './model';
-import locationView from './views/locationView';
-import searchView from './views/searchView';
 import mapView from './views/mapView';
+import searchView from './views/searchView';
+import locationView from './views/locationView';
+import markerIcon from '../images/icon-location.svg';
 
 const controlAddress = async () => {
   try {
@@ -24,69 +23,6 @@ const controlAddress = async () => {
     console.log(`${err} 🤦‍♂️`);
   }
 };
-
-// const getLatLng = async ip => {
-//   console.log(data);
-
-//   const markup = `
-//     <address class="data">
-//                 <p>ip address</p>
-//                 <p>${data.ip}</p>
-//             </address>
-
-//             <address class="data">
-//                 <p>location</p>
-//                 <p>${data.location.city}, ${data.location.country} ${data.location.postalCode}</p>
-//             </address>
-
-//             <time class="data">
-//                 <p>timezone</p>
-//                 <p>utc ${data.location.timezone}</p>
-//             </time>
-
-//             <address class="data">
-//                 <p>isp</p>
-//                 <p>${data.isp}</p>
-//             </address>`;
-
-//   infoContainer.insertAdjacentHTML('beforeend', markup);
-//   return data.location;
-// };
-// let map;
-// const displayMap = async ip => {
-//   mapContainer.innerHTML = '';
-//   mapContainer.setAttribute('class', '');
-
-//   const { lat, lng } = await getLatLng(ip);
-//   const coords = [lat, lng];
-//   console.log(coords);
-
-//   if (map) {
-//     map.remove();
-//     map.off();
-//   }
-
-//   map = L.map('map').setView(coords, 16);
-
-//   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution:
-//       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//   }).addTo(map);
-
-//   const myIcon = L.icon({
-//     iconUrl: markerIcon,
-//     iconSize: [50, 60],
-//     iconAnchor: [30, 60],
-//     popupAnchor: [-3, -76],
-//   });
-
-//   L.marker(coords, { icon: myIcon }).addTo(map);
-// };
-// form.addEventListener('submit', function (e) {
-//   e.preventDefault();
-//   //displayMap(ip);
-//   e.target.firstElementChild.value = '';
-// });
 
 const init = () => {
   searchView.addHandlerSearch(controlAddress);
