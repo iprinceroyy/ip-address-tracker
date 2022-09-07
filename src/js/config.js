d6c9708ea0fb902cd13fe0ba9395657ee544d9e6
Prince Roy <princeku.ray143@gmail.com>
2022-09-07 17:09:59 +0530

    update API_KEY

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const IP_API_URL = `https://geo.ipify.org/api/v2/country,city`;
 export const MAP_URL = `https://tile.openstreetmap.org/{z}/{x}/{y}.png`;
 export const ATTRIBUTION = `https://www.openstreetmap.org/copyright`;
-export const API_KEY = `at_IZf4myTNbJ4JraT6vCx5rR7u0sBpg`;
+export const API_KEY = `at_o54tBe66iCSIGA7h6baJAq2ksgi5J`;
 export const TIMEOUT_SEC = 10;
